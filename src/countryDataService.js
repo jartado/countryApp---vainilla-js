7ef1351f2e0a0628e4f2bb4db1de444557076fcc
Javier Hurtado <javier.hurtado@quintasenergy.com>
2022-12-19 16:17:21 +0100

    Docs y propiedad APIurl

--- src/countryDataService.js
+++ src/countryDataService.js
@@ -1,23 +1,25 @@
 
 export class CountryDataService {
 
+    #APIurl = 'https://restcountries.com/v3.1/'
+
     async getCountryDataAll() {
-        const url = "https://restcountries.com/v3.1/all"
+        const url = this.#APIurl + "all"
         return await this.#requestCountries(url)
     }
 
     async getCountryDataByCode(code) {
-        const url = "https://restcountries.com/v3.1/alpha/" + code
+        const url = this.#APIurl + "alpha/" + code
         return await this.#requestCountries(url, true)
     }
 
     async getCountryDataByRegion(region) {
-        const url = "https://restcountries.com/v3.1/region/" + region
+        const url = this.#APIurl + "region/" + region
         return await this.#requestCountries(url)
     }
 
     async getCountryDataBySubregion(subregion) {
-        const url = "https://restcountries.com/v3.1/subregion/" + subregion
+        const url = this.#APIurl + "subregion/" + subregion
         return await this.#requestCountries(url)
     }
 
